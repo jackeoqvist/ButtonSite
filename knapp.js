fd35ef367de92918e188efca42a72217aafdd24a
jackeoqvist <jacke98@hotmail.se>
2015-06-27 01:05:04 +0200

    added some stuff

--- knapp.js
+++ knapp.js
@@ -5,13 +5,30 @@ var knapp = function(){
 	var funktioner = 
 		[function() {
 			document.getElementById("demo").innerHTML = "Greattastic";
+			
 		},
 		function() {
 			document.getElementById("demo").innerHTML = "Awesomeness";
+			
 		},
 		function() {
 			document.getElementById("demo").innerHTML = "Crazyness";
+			
+		},
+		
+		function() {
+			window.alert("Hello to you sir!")
+		},
+
+		function() {
+			var div = document.getElementById("demo");
+			var img = document.createElement('img');
+
+			img.src = 'http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr01/30/12/anigif_enhanced-buzz-14674-1383151956-4.gif';
+  			div.appendChild(img);
 		}
+
+
 
 		];
 
