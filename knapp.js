ac7fc0cac4673ff689555c8c22a5840611ff1630
jackeoqvist <jacke98@hotmail.se>
2015-06-29 09:38:44 +0200

    added iframe to script

--- knapp.js
+++ knapp.js
@@ -16,9 +16,9 @@ var knapp = function(){
 			
 		},
 		
-		function() {
-			window.alert("Hello to you sir!")
-		},
+		/*	function() {
+				window.alert("Hello to you sir!")
+			}, */
 
 		function() {
 			document.getElementById("demo").innerHTML = "";
@@ -26,6 +26,18 @@ var knapp = function(){
 			var img = document.createElement('img');
 			img.src = 'http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr01/30/12/anigif_enhanced-buzz-14674-1383151956-4.gif';
   			div.appendChild(img);
+		},
+
+		function() {
+			document.getElementById("demo").innerHTML = "";
+			var div = document.getElementById("demo")
+			var iframe = document.createElement('iframe')
+	
+			iframe.src = "https://www.youtube.com/embed/B-N1yJyrQRY?autoplay=1";
+			iframe.width = 500;
+			iframe.height = 500;
+			iframe.allowfullscreen = true;
+			div.appendChild(iframe);
 		}
 
 
